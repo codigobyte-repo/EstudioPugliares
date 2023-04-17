e13373297fcab4c8801045d8d717ce3ae19114eb
Martin <maquino@codigobyte.com.ar>
2023-04-17 08:44:25 -0300

    Agregue el ruteo de post getRouteKeyname

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,6 +11,11 @@ class Post extends Model
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
+    public function getRouteKeyName()
+    {
+        return "slug";
+    }
+
     const BORRADOR = 1;
     const PUBLICADO = 2;
 
